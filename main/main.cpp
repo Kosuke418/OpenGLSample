6f0c70fe1c2db466255acb67cbe2b276d2e34af8
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-12-13 00:39:09 +0900

    Initial Commit

---
+++ main/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  main
+//
+//  Created by 西川功佑 on 2019/12/13.
+//  Copyright © 2019 西川功佑. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
